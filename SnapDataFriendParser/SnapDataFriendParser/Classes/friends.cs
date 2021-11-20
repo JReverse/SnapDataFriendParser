c54ff3b02ff45999f325ced31f03bd24be2e9791
J <40630518+JReverse@users.noreply.github.com>
2021-11-20 07:51:46 -0600

    Init Commit

---
+++ SnapDataFriendParser/SnapDataFriendParser/Classes/friends.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+
+namespace SnapDataFriendParser.Classes
+{
+    public class Friend
+    {
+        public string Username { get; set; }
+
+        [JsonProperty("Display Name")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("Creation Timestamp")]
+        public string CreationTimestamp { get; set; }
+
+        [JsonProperty("Last Modified Timestamp")]
+        public string LastModifiedTimestamp { get; set; }
+        public string Source { get; set; }
+    }
+
+    public class FriendRequestsSent
+    {
+        public string Username { get; set; }
+
+        [JsonProperty("Display Name")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("Creation Timestamp")]
+        public string CreationTimestamp { get; set; }
+
+        [JsonProperty("Last Modified Timestamp")]
+        public string LastModifiedTimestamp { get; set; }
+        public string Source { get; set; }
+    }
+
+    public class BlockedUser
+    {
+        public string Username { get; set; }
+
+        [JsonProperty("Display Name")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("Creation Timestamp")]
+        public string CreationTimestamp { get; set; }
+
+        [JsonProperty("Last Modified Timestamp")]
+        public string LastModifiedTimestamp { get; set; }
+        public string Source { get; set; }
+    }
+
+    public class DeletedFriend
+    {
+        public string Username { get; set; }
+
+        [JsonProperty("Display Name")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("Creation Timestamp")]
+        public string CreationTimestamp { get; set; }
+
+        [JsonProperty("Last Modified Timestamp")]
+        public string LastModifiedTimestamp { get; set; }
+        public string Source { get; set; }
+    }
+
+    public class HiddenFriendSuggestion
+    {
+        public string Username { get; set; }
+
+        [JsonProperty("Display Name")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("Creation Timestamp")]
+        public string CreationTimestamp { get; set; }
+
+        [JsonProperty("Last Modified Timestamp")]
+        public string LastModifiedTimestamp { get; set; }
+        public string Source { get; set; }
+    }
+
+    public class Shortcut
+    {
+        [JsonProperty("Shortcut Name")]
+        public string ShortcutName { get; set; }
+        public string Created { get; set; }
+    }
+
+    public class FriendKlass
+    {
+        public List<Friend> Friends { get; set; }
+
+        [JsonProperty("Friend Requests Sent")]
+        public List<FriendRequestsSent> FriendRequestsSent { get; set; }
+
+        [JsonProperty("Blocked Users")]
+        public List<BlockedUser> BlockedUsers { get; set; }
+
+        [JsonProperty("Deleted Friends")]
+        public List<DeletedFriend> DeletedFriends { get; set; }
+
+        [JsonProperty("Hidden Friend Suggestions")]
+        public List<HiddenFriendSuggestion> HiddenFriendSuggestions { get; set; }
+
+        [JsonProperty("Ignored Snapchatters")]
+        public List<object> IgnoredSnapchatters { get; set; }
+
+        [JsonProperty("Pending Requests")]
+        public List<object> PendingRequests { get; set; }
+        public List<Shortcut> Shortcuts { get; set; }
+    }
+
+
+}
---
+++ SnapDataFriendParser/SnapDataFriendParser/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using SnapDataFriendParser.Classes;
+
+FriendKlass FriendKlass = JsonConvert.DeserializeObject<FriendKlass>(File.ReadAllText("friends.json")) ?? throw new Exception("File Cant Be Empty"); 
+
+foreach (Friend Username in FriendKlass.Friends)
+{
+    using (var stream = new StreamWriter("Friends.txt", true))
+    {
+        stream.WriteLine(Username.Username);
+    }
+}
+
+foreach (BlockedUser Username in FriendKlass.BlockedUsers)
+{
+    using (var stream = new StreamWriter("Blocked.txt", true))
+    {
+        stream.WriteLine(Username.Username);
+    }
+}
+
+Console.WriteLine("Done!");
+Console.ReadLine();
